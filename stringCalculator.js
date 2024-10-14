41382264c653acc503042d9e36052c0c2d52d983
Dinesh <dineshkokate18@gmail.com>
2024-10-14 23:37:11 +0530

    test case: adding 2 numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,8 @@
 function add(numbers) {
     if (numbers === "") return 0;
 
+    const numArray = numbers.split(/,|\n/).map(Number);
+    return numArray.reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = { add };
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -5,4 +5,16 @@ describe('check add function',()=>{
     test('should return 0 for an empty string', () => {
         expect(add("")).toBe(0);
     });
+
+    // Test for a single number
+    test('should return the number itself for a single number string', () => {
+        expect(add("1")).toBe(1);
+    });
+
+    // Test for two numbers
+    test('should return the sum of two numbers separated by a comma', () => {
+        expect(add("1,2")).toBe(3);
+    });
+
 })
+
